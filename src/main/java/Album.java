f042494c436c2a9cd490ca2e9fede84486be93d4
Juan Longoria <juan.a.longoria66@gmail.com>
2022-02-07 12:04:28 -0600

    working on javabeans exercises

---
+++ src/main/java/Album.java
@@ -0,0 +1,18 @@
+import java.util.Date;
+
+public class Album {
+    private long id;
+    private String artist;
+    private String name;
+    private int releaseDate;
+    private float sales;
+    private String genre;
+
+    public Album() {
+    }
+
+
+
+
+
+}
---
+++ src/main/java/BeanTester.java
@@ -0,0 +1,24 @@
+//public class BeanTester {
+//    public static void main(String[] args) {
+//        Album album = new Album(1, "Michael Jackson", "Dirty Diana", 1986, 2.69, "Rock");
+//        Author shrek = new Author(1, "Shrek", "An Ogre");
+//        Quote quote = new Quote(1, "Better out than in, I always say.", shrek);
+//
+//        List<Album> collectionOfAlbums = new ArrayList<>();
+//        collectionOfAlbums.add(bts);
+//        collectionOfAlbums.add(unwritten);
+//
+//        for (Album album : collectionOfAlbums) {
+//            System.out.println("Favorite albums is %s by the music act %s, which has sold %2f million copies.", album.getName(), album.getArtist(), album.getSales());
+//        }
+//
+//        System.out.println("The wise philosopher %s %s once said \"%s\"%n", quote.getAuthor().getFirstName(), quote.getAuthor().getLastName(), quote.getContent());
+//
+////Dates in Java
+//
+//        Date date = new Date("");
+//        System.out.println(date);
+//
+//
+//    }
+
